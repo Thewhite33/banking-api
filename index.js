d4d149e54489c046bef80ea8af456c15ae0f20bb
Rohit Chakraborty <58523433+Thewhite33@users.noreply.github.com>
2024-06-16 13:40:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ dbConnect()
 app.use('/api',bankRoutes)
 
 app.get('/',(req,res)=>{
-    res.send('<h1>Banking API</h1><p>Search /api/banks to get all banks details</p> <p>Search /bankname to get all corresponding bank</p> <p>Search /bankname/branchname to get details of particular bank branchname</p>')
+    res.send('<h1>Banking API</h1><p>Search /api/banks to get all banks details</p> <p>Search /api/bankname to get all corresponding bank</p> <p>Search /api/bankname/branchname to get details of particular bank branchname</p>')
 })
 
 app.listen(PORT,()=>{
